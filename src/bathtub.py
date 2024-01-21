fixed signs

src/bathtub.py
@@ -18,7 +18,7 @@ class Bathtub(Plant):
             
             velocity = jnp.sqrt(2 * self.g * self.height)
             flow_rate = velocity * self.c
-            volume_change = control_signal + disturbance - flow_rate
+            volume_change = - control_signal + disturbance - flow_rate
             self.height += volume_change / self.area
             
             # Ensure height does not go negative after update

src/consys.py
@@ -53,7 +53,7 @@ class ConSys:
             params_list.append(params_jax)
             mse, gradients = gradfunc(params_jax)
             print(mse, params_jax)
-            params_jax = params_jax + self.learning_rate * gradients
+            params_jax = params_jax - self.learning_rate * gradients
             mse_list.append(mse)
             
         params_matrix = jnp.array(params_list)